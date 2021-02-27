146a301a3112fad1eb26b83139348befb389f605
Danila Engel <engeldanila@mail.ru>
2021-02-27 10:46:41 +0300

    already better than it was

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,12 @@ const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const {errorLogger} = require('./middlewares/logger');
 const path = require("path")
+var cors = require("cors")
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('./routes/index'));
